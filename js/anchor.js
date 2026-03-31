99b9a465ea52a9c816885545db1591d3cf7652fc
myc <60999294+hello-myc@users.noreply.github.com>
2026-03-31 18:07:19 +0800

    remove anchor listenser

--- js/anchor.js
+++ js/anchor.js
@@ -15,31 +15,30 @@ $('.js-anchor-link').click(function(e){
 	Smooth scroll functionality for anchor links (animates the scroll
 	rather than a sudden jump in the page)
 */
-$('.js-anchor-link').click(function(e){
-  e.preventDefault();
-  var target = $($(this).attr('href'));
-  if(target.length){
-    var scrollTo = target.offset().top;
-    $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
-  }
-});
 
-/*
-  Nav active state on scroll
-*/
-const sections = document.querySelectorAll('#section-2, #section-1, #section-4, #section-5');
+// $('.js-anchor-link').click(function(e){
+//   e.preventDefault();
+//   var target = $($(this).attr('href'));
+//   if(target.length){
+//     var scrollTo = target.offset().top;
+//     $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+//   }
+// });
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      document.querySelectorAll('.btn-box a').forEach(a => a.classList.remove('active'));
-      const id = entry.target.getAttribute('id');
-      const activeLink = document.querySelector(`.btn-box a[href="#${id}"]`);
-      if (activeLink) activeLink.classList.add('active');
-    }
-  });
-}, {
-  threshold: 0.2
-});
 
-sections.forEach(section => observer.observe(section));
+// const sections = document.querySelectorAll('#section-2, #section-1, #section-4, #section-5');
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       document.querySelectorAll('.btn-box a').forEach(a => a.classList.remove('active'));
+//       const id = entry.target.getAttribute('id');
+//       const activeLink = document.querySelector(`.btn-box a[href="#${id}"]`);
+//       if (activeLink) activeLink.classList.add('active');
+//     }
+//   });
+// }, {
+//   threshold: 0.2
+// });
+
+// sections.forEach(section => observer.observe(section));
